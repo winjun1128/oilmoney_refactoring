f3958b826eac2835074f4da091966fad5dbdef1f
winjun1128 <hsj1128ty@naver.com>
2025-09-08 13:28:27 +0900

    목적지 반응형 추가

--- src/main/frontend/src/page/RouteMapPage.js
+++ src/main/frontend/src/page/RouteMapPage.js
@@ -1,19 +1,18 @@
 import RouteMap from "../components/RouteMap";
 import RouteSideBar from "./RouteSideBar";
 import SideBar from "./RouteSideBar";
-
+// RouteMapPage.jsx
 export default function RouteMapPage({ isLogin, setIsLoginModalOpen }) {
-    return (
-        <div style={{ display: "flex", height: "100vh" }}>
-            {/* 왼쪽 구역 */}
-            <div style={{ width: "80px", background: "#f3f4f6", borderRight: "1px solid black" }}>
-                <RouteSideBar isLogin={isLogin} setIsLoginModalOpen={setIsLoginModalOpen}/>
-            </div>
+  return (
+    <div className="layout">
+      <aside className="app-nav">
+        <RouteSideBar isLogin={isLogin} setIsLoginModalOpen={setIsLoginModalOpen}/>
+      </aside>
 
-            {/* 오른쪽 구역 */}
-            <div style={{ flex: 1, background: "#ffffff", position: "relative" }}>
-                <RouteMap/>
-            </div>
-        </div>
-    );
+      <main className="app-main">
+        <RouteMap/>
+      </main>
+    </div>
+  );
 }
+
